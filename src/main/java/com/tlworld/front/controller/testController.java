c0e54fd84568c5612ba05658f0a4d3e5d428c81a
jshblack <118979741+JSH1990@users.noreply.github.com>
2025-01-24 09:49:02 +0900

    test2

--- src/main/java/com/tlworld/front/controller/testController.java
+++ src/main/java/com/tlworld/front/controller/testController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class testController {
     @GetMapping("/test")
     public String test(){
+        //test
         return "test";
     }
 }
